d5cc92458ccc0170e1156496346bd00bc42ff048
Aris Papadopoulos <aristides.papadopoulos@gmail.com>
2018-10-14 11:46:45 +0200

    Improved Random provider and test

--- app/src/main/java/app/we/go/emojidraw/data/RandomEmojiToDrawProvider.kt
+++ app/src/main/java/app/we/go/emojidraw/data/RandomEmojiToDrawProvider.kt
@@ -14,32 +14,19 @@ class RandomEmojiToDrawProvider(private val emojiList: List<Emoji>,
     private val random: Random = Random()
 
     override fun provide(n: Int): List<EmojiToDraw> {
-        val list = ArrayList<EmojiToDraw>()
-        for (i in 0 until n) {
-            list.add(getEmojiChallengeNotInList(list))
-        }
-        return list
+
+        return (1..MAX_TRIES).map {
+                    emojiList[random.nextInt(emojiList.size)]
+                }
+                .filter {
+                    it.sinceVersion <= sdkVersion
+                }
+                .distinct()
+                .take(n)
+                .map {
+                    EmojiToDraw(it.description, it.emoji)
+                }
+
     }
-
-
-    private fun getEmojiChallengeNotInList(list: List<EmojiToDraw>): EmojiToDraw {
-        for (i in 1..MAX_TRIES) {  // avoid going into infinite loop
-            val emojiToDraw = provideOne()
-            if (!list.contains(emojiToDraw)) return emojiToDraw
-        }
-
-        return provideOne() //we have exhausted MAX_TRIES, just get some emoji and return it, will probably be a duplicate...
-    }
-
-    private fun provideOne(): EmojiToDraw {
-        var emoji : Emoji
-        do {
-            emoji = emojiList[random.nextInt(emojiList.size)]
-        } while (!isVersionSatisfied(emoji))
-        return EmojiToDraw(emoji.description, emoji.emoji)
-    }
-
-    private fun isVersionSatisfied(emoji: Emoji) = emoji.sinceVersion <= sdkVersion
-
 
 }
